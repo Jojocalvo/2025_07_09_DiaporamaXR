dd107c1aad74e0376054a6072c4d6a9d532a15cf
Jojocalvo <geoffrey.lhomme01@gmail.com>
2025-07-09 14:23:17 +0200

    Save: 2025_07_09_14_23

--- Runtime/DiapoMono_RemoveDefaultUnityCamera.cs
+++ Runtime/DiapoMono_RemoveDefaultUnityCamera.cs
@@ -1,32 +1,39 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-
 public class DiapoMono_RemoveDefaultUnityCamera : MonoBehaviour
 {
- 
+    public string[] m_ignoreThoseCamera = new string[] {
+        "RightEyeAnchor", "CenterEyeAnchor", "LeftEyeAnchor"
+    };
+
     [ContextMenu("Remove Camera")]
     public void RemoveDefaultUnityCamera()
     {
         // Find all cameras in the scene
         Camera[] cameras = Camera.allCameras;
 
+        bool cameraDestroyed = false;  
+
         foreach (Camera camera in cameras)
         {
-            // Check if the camera's name is "Main Camera"
-            if (camera.gameObject.name == "Main Camera")
-            {
-                // Destroy the camera's game object
-                if (Application.isPlaying) 
-                    Destroy(camera.gameObject);
-                 else  
-                        DestroyImmediate(camera.gameObject);
+            // Ignore cameras that match the specified names
+            if (System.Array.Exists(m_ignoreThoseCamera, name => name == camera.name))
+                continue;
 
-                return; // Exit after destroying the camera
-            }
+            // Destroy the camera's game object
+            if (Application.isPlaying)
+                Destroy(camera.gameObject);
+            else
+                DestroyImmediate(camera.gameObject);
+
+            cameraDestroyed = true;  // Set flag to true if a camera was destroyed
         }
 
-        // If no camera named "Main Camera" is found
-        Debug.Log("No camera named 'Main Camera' found in the scene.");
+        // If no camera was destroyed
+        if (!cameraDestroyed)
+        {
+            Debug.Log("No camera named 'Main Camera' found in the scene, or all cameras are ignored.");
+        }
     }
 }
